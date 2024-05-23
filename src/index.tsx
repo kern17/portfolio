31ffd7a04a169542ae01376da1079b9b0e3d65c6
kern17 <kern.patrick17@outlook.de>
2024-05-23 11:50:13 +0100

    temp-commit

---
+++ src/index.tsx
@@ -0,0 +1,40 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './components/app'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+// import { StrictMode } from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './index.css'
+// const a: React;
+
+
+// const root = createRoot(document.getElementById('root')!);
+// root.render(
+//     <StrictMode>
+      
+//       {/* <MyComponent />   */}
+//       <div>test</div>
+//     </StrictMode>
+// );
+
+// .render(
+//   <StrictMode>
+    
+//     {/* <MyComponent />   */}
+//     <div>test</div>
+//   </StrictMode>
+// )
+
+// const MyComponent: React.FC = (): React.ReactNode => {
+//   return <span>This is fine</span>;
+// };
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: "./src",
+  publicDir: "../public",
+  envDir: "../env",
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    copyPublicDir: true
+  }
 })
